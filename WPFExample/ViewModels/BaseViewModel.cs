e9b22fedc883d7bd5a3eeae505afd05488003413
eXCoreX <excoreky@gmail.com>
2021-04-20 10:58:34 +0300

    first commit

---
+++ WPFExample/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace WPFExample.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ WPFExample/ViewModels/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Input;
+using WpfMandelbrotDrawer.Commands;
+
+namespace WPFExample.ViewModels
+{
+    class MainViewModel : BaseViewModel
+    {
+        private string _MyData = "test";
+        public string MyData
+        {
+            get { return _MyData; }
+            set
+            {
+                _MyData = value;
+                OnPropertyChanged(nameof(MyData));
+            }
+        }
+
+        private ICommand changeToTest;
+
+        public ICommand ChangeToTest
+        {
+            get
+            {
+                if (changeToTest == null)
+                {
+                    changeToTest = new RelayCommand(PerformChangeToTest, CanPerformChangeToTest);
+                }
+
+                return changeToTest;
+            }
+        }
+
+        private bool CanPerformChangeToTest(object obj)
+        {
+            return MyData != "test";
+        }
+
+        private void PerformChangeToTest(object commandParameter)
+        {
+            MyData = "test";
+        }
+    }
+}
